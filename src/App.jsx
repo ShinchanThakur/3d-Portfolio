4b9f6cc683416202211fadce3547c149f4ee0bc8
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-15 16:42:38 +0530

    Added pages for each nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import './index.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from './pages/index';
 
 const App = () => {
     return (
@@ -8,10 +9,10 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={'Home'} />
-                    <Route path="/about" element={'About'} />
-                    <Route path="/projects" element={'Projects'} />
-                    <Route path="/contact" element={'Contact'} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </main>
