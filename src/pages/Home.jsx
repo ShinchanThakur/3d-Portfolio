80d6828af3222d8a5082453781d4a78a2499563e
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-15 18:50:33 +0530

    Added island to our home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
                 POPUP
@@ -12,7 +13,14 @@ const Home = () => {
             <Canvas
                 className="w-full h-screen bg-transparent"
                 camera={{ near: 0.1, far: 1000 }}>
-                <Suspense fallback={<Loader />}></Suspense>
+                <Suspense fallback={<Loader />}>
+                    <directionalLight />
+                    <ambientLight />
+                    <pointLight />
+                    <spotLight />
+                    <hemisphereLight />
+                    <Island />
+                </Suspense>
             </Canvas>
         </section>
     );
