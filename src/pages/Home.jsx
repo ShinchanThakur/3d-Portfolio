387be6ea8ec7d29ad2e32c90dd049252bebb2854
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-17 10:58:06 +0530

    Added cursor to grab island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
@@ -11,6 +11,8 @@ import Plane from "../models/Plane";
             </div> */}
 
 const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = [1, 1, 1];
         let screenPosition = [0, -6.5, -43];
@@ -27,7 +29,7 @@ const Home = () => {
     return (
         <section className="w-full h-screen relative">
             <Canvas
-                className="w-full h-screen bg-transparent"
+                className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}>
                 <Suspense fallback={<Loader />}>
                     <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -39,6 +41,8 @@ const Home = () => {
                         position={islandPosition}
                         scale={islandScale}
                         rotation={islandRotation}
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
                     />
                     <Plane />
                 </Suspense>
