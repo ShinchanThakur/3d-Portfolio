9f0b67097aa569c11c91c7b8b253bd530e9c061f
Rohan Thakur <65813683+ShinchanThakur@users.noreply.github.com>
2023-11-17 14:22:54 +0530

    Merge rotation to master (#5)
    * Added cursor to grab island
    
    * Adjusted plane position according to screen size
    
    * Added rotation using mouse
    
    * Added rotation using keyboard
    
    * Added rotation according to each frame
    
    * Corrected and added event listener to canvas instead of dom
    
    * Corrected handle pointer functions in island model
    
    * Made the rotation of island smooth by slowing down speed when mouse pointer is not moving
    
    * Added dummy value for setCurrentStage in Island model
    
    * Rotating plane blades with mouse movement
    
    * Added rotation to sky
    
    * Added credits for other 3d models
    
    * Bird flapping its wings
    
    * Bird moving in Sin motion in y axis
    
    * Changing bird flight direction when it reaches end of screen
    
    * Changing bird position in x and z axis according to its direction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
@@ -11,6 +11,9 @@ import Plane from "../models/Plane";
             </div> */}
 
 const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+    const [currentStage, setCurrentStage] = useState(1);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = [1, 1, 1];
         let screenPosition = [0, -6.5, -43];
@@ -22,25 +25,49 @@ const Home = () => {
         return [screenScale, screenPosition, rotation];
     }
 
+    const adjustPlaneForScreenSize = () => {
+        let screenScale = [3, 3, 3];
+        let screenPosition = [0, -4, -4];
+        const screenRotation = [0, 20, 0];
+
+        if (window.innerWidth < 768) {
+            screenScale = [1.5, 1.5, 1.5];
+            screenPosition = [0, -1.5, 0];
+        }
+
+        return [screenScale, screenPosition, screenRotation];
+    }
+
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+    const [planeScale, planePosition, planeRotation] = adjustPlaneForScreenSize();
 
     return (
         <section className="w-full h-screen relative">
             <Canvas
-                className="w-full h-screen bg-transparent"
+                className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}>
                 <Suspense fallback={<Loader />}>
                     <directionalLight position={[1, 1, 1]} intensity={2} />
                     <ambientLight intensity={0.5} />
                     <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
                     <Bird />
-                    <Sky />
+                    <Sky
+                        isRotating={isRotating}
+                    />
                     <Island
                         position={islandPosition}
                         scale={islandScale}
                         rotation={islandRotation}
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
+                        setCurrentStage={setCurrentStage}
                     />
-                    <Plane />
+                    <Plane
+                        isRotating={isRotating}
+                        planeScale={planeScale}
+                        planePosition={planePosition}
+                        rotation={planeRotation}
+                    />
                 </Suspense>
             </Canvas>
         </section>
