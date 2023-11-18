3bbfe6ee77f8b1cd3a321e44e9786a009fb5d5ab
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-18 10:03:08 +0530

    Added infoBox component as popup helper

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain" />
+        </Link>
+    </div>
+)
+
 const renderContent = {
     1: (
         <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
@@ -7,7 +20,11 @@ const renderContent = {
         </h1>
     ),
     2: (
-        <h1>2</h1>
+        <InfoBox
+            text="Worked with many companies and picked up many skills along the way"
+            link="/about"
+            btnText="Learn more"
+        />
     ),
     3: (
         <h1>3</h1>
