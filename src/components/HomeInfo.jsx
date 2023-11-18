2e2507014e93bfcee8629758f67648910a00b80a
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-18 09:53:00 +0530

    Added name popup

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,6 +1,10 @@
 const renderContent = {
     1: (
-        <h1>1</h1>
+        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+            Hi, I am <span className="font-semibold">Rohan</span>👋
+            <br />
+            A Software Engineer from India
+        </h1>
     ),
     2: (
         <h1>2</h1>
