6fb775bbb17cc448f2f30bd52af1ea2cc1385baf
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-18 08:55:54 +0530

    Added div for popup in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,6 @@ import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
 
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-                POPUP
-            </div> */}
-
 const Home = () => {
     const [isRotating, setIsRotating] = useState(false);
     const [currentStage, setCurrentStage] = useState(1);
@@ -43,6 +39,9 @@ const Home = () => {
 
     return (
         <section className="w-full h-screen relative">
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                POPUP
+            </div>
             <Canvas
                 className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}>
