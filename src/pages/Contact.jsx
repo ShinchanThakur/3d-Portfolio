52cc1c55b0f0f405cb8ae77ed5c41aa9d24d8e2c
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-19 16:02:16 +0530

    Added send message button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+    const formRef = useRef(null);
     const [form, setForm] = useState({ name: '', email: '', message: '' });
+    const [isLoading, setIsLoading] = useState(false);
     const handleChange = () => { };
     const handleFocus = () => { };
     const handleBlur = () => { };
+    const handleSubmit = () => { };
     return (
         <section className="relative flex lg:flex-row flex-col max-container">
             <div className="flex-1 min-w-[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
 
-                <form className="w-full flex flex-col gap-7 mt-14">
+                <form
+                    className="w-full flex flex-col gap-7 mt-14"
+                    onSubmit={handleSubmit}>
                     <label className="text-black-500 font-semibold">
                         Name
                         <input
@@ -53,6 +58,15 @@ const Contact = () => {
                             onBlur={handleBlur}
                         />
                     </label>
+                    <button
+                        className="btn"
+                        type="submit"
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        disabled={isLoading}
+                    >
+                        {isLoading ? 'Sending...' : 'Send Message'}
+                    </button>
                 </form>
             </div>
         </section>
