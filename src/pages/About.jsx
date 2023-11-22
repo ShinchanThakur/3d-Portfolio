13923f2b286bbb6ca7ab901a1b4868bcd267c19d
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-22 09:55:58 +0530

    Added skills

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import { skills } from "../constants";
+
 const About = () => {
     return (
         <section className="max-container">
@@ -8,6 +10,26 @@ const About = () => {
             <div className="mt-5 flex flex-col gap-3 text-slate-500">
                 <p>Software Developer based in India, specializing in full-stack development. I'm also a competitive programmer and take part in coding contests regularly.</p>
             </div>
+
+            <div className="py-10 flex flex-col">
+                <h3 className="subhead-text">My Skills</h3>
+
+                <div className="mt-16 flex flex-wrap gap-12">
+                    {skills.map((skill) => (
+                        <div className="block-container w-20 h-20" key={skill.name}>
+                            <div className="btn-back rounded-xl" />
+                            <div className="btn-front rounded-xl flex justify-center items-center">
+                                <img
+                                    src={skill.imageUrl}
+                                    alt={skill.name}
+                                    className="w-1/2 h-1/2 object-contain"
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </section>
     );
 };
