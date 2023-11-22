59d432ebda7df4c01e3cd756674457437972ee14
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-22 09:38:19 +0530

    Added name and bio-line

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
 const About = () => {
     return (
-        <div>About</div>
+        <section className="max-container">
+            <h1 className="head-text">
+                Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow">Rohan</span>
+            </h1>
+
+            <div className="mt-5 flex flex-col gap-3 text-slate-500">
+                <p>Software Developer based in India, specializing in full-stack development. I'm also a competitive programmer and take part in coding contests regularly.</p>
+            </div>
+        </section>
     );
 };
 
