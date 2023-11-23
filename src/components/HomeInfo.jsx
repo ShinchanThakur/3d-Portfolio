e29bdca7f6fdd2b68548207d2f196b6027edbf1f
Rohan Thakur <65813683+ShinchanThakur@users.noreply.github.com>
2023-11-23 09:19:21 +0530

    Add Work Experience to About page (#9)
    * Added name and bio-line
    
    * Added skills in constants
    
    * Added skills
    
    * Added config details for email setup in readme
    
    * Informing the user that the island can be rotated
    
    * Added timeline component and Work experience section
    
    * Added work experience in constants
    
    * Modified exports in constants folder
    
    * Added work experience to about page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -13,11 +13,15 @@ const InfoBox = ({ text, link, btnText }) => (
 
 const renderContent = {
     1: (
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
-            Hi, I am <span className="font-semibold">Rohan</span>👋
-            <br />
-            A Software Engineer from India
-        </h1>
+        <>
+            <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+                Hi, I am <span className="font-semibold">Rohan</span>👋
+                <br />
+                A Software Engineer from India
+                <br />
+                <span className="text-xs">Try rotating the island</span>
+            </h1>
+        </>
     ),
     2: (
         <InfoBox
