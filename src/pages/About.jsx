e357331dfbb126b37ae62d3153d1178ca2325c84
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-23 08:09:25 +0530

    Added timeline component and Work experience section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { skills } from "../constants";
 
 const About = () => {
@@ -30,6 +32,15 @@ const About = () => {
                 </div>
             </div>
 
+            <div className="py-16">
+                <h3 className="subhead-text">Work Experience</h3>
+                <div className="mt-5 flex flex-col gap-3 text-slate-500">
+                    <p>I've worked with all sorts of companies, leveling up my skills and teaming up with smart people. Here's the rundown:</p>
+                </div>
+
+
+
+            </div>
         </section>
     );
 };
