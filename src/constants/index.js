18e1f6c6bc64440e2f3d0c9f5a19a55aa117928b
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-23 08:31:51 +0530

    Added work experience in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+import { deutscheBank, bnyMellon } from "../assets/images";
 import {
     css,
     express,
@@ -6,15 +7,9 @@ import {
     html,
     javascript,
     mongodb,
-    motion,
-    mui,
-    nextjs,
     nodejs,
     react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript
+    redux
 } from "../assets/icons";
 
 export const skills = [
@@ -98,4 +93,42 @@ export const skills = [
     //     name: "TypeScript",
     //     type: "Frontend",
     // }
+];
+
+export const experiences = [
+    {
+        title: "SDE Intern",
+        company_name: "Deutsche Bank",
+        icon: deutscheBank,
+        iconBg: "#F5F5F5",
+        date: "Jun 2021 - Aug 2021",
+        points: [
+            "Created a pipeline to fetch Enron Dataset containing 5 lakh records and indexed into Solr",
+            "Created REST APIs for messages and developed UI to query the data ingested in Solr based on parameters",
+            "Using Jenkins (CI/CD) setup built and deployed the project on Tomcat",
+            "Reduced the Time complexity of data cleaning by 10 times ,30 min to 3 min",
+            "Technologies Used : Python | Apache Solr | SpringBoot | Angular",
+        ],
+    },
+    {
+        title: "Software Developer",
+        company_name: "Bank ofNew York Mellon",
+        icon: bnyMellon,
+        iconBg: "#000000",
+        date: "Jul 2022 - Sep 2023",
+        points: [
+            "Worked on a keystone project in Shared Services",
+            "Designing and building a production-level web application",
+            "Worked with Clients to take input specs and resolve their issues",
+            "Rolled out Self service capabilities for business to create and test rules for reconciliation",
+            "Migrated project from Monolith architecture to microfrontends and micro-services architecture.",
+            "Built REST APIs for enhancing authentication, logging, throttling, metering and other analytics features",
+            "Acquired an extensive proficiency in front-end architecture and implemented data-driven development methodologies.",
+            "Wrote test cases and expand test coverage for the code to maintain code quality.",
+            "Experienced in Scrum Agile methodology, driving crossfunctional collaboration and delivering high-quality software solutions through effective sprint planning, backlog grooming, and sprint reviews.",
+            "Familiarity working with UX and Visual Designers",
+            "Handled Prod deployment and resolved issues",
+            "Technologies - Spring | React | Drools | DSL",
+        ],
+    },
 ];
